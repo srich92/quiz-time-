d395974769dfc4e4bd581b9277aa9bfbe9cd9eb7
srich92 <srichards646@gmail.com>
2022-08-11 12:36:29 -0400

    added questions

--- assets/script.js
+++ assets/script.js
@@ -1,25 +1,30 @@
-function show(time = 1) {
-  var startcontainer = document.querySelector("#start-btn");
-  var quizcontainer = document.querySelector("#Quiz");
-  var HighScorecontainer = document.querySelector("#High-Score");
 
-  quizcontainer.hidden = true;
-  HighScorecontainer.hidden = true;
-  startcontainer.hidden = true;
+// Questions
+var questions = [
+  {
+      title: "Commonly used data types DO NOT include:",
+      choices: ["strings", "booleans", "alerts", "numbers"],
+      answer: "alerts"
+  },
+  {
+      title: "The condition in an if / else statement is enclosed within ____.",
+      choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+      answer: "parentheses"
+  },
+  {
+      title: "Arrays in Javascript can be used to store ____.",
+      choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+      answer: "all of the above"
+  },
+  {
+      title: "String values must be enclosed within ____ when being assigned to variables.",
+      choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+      answer: "quotes"
+  },
+  {
+      title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+      choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+      answer: "console log"
+  },
 
-  if (time == 0) startcontainer.hidden = false;
-  if (time == 1) {
-    quizcontainer.hidden = false;
-  }
-  if (time == 2) {
-    HighScorecontainer.hidden = false;
-  }
-}
-show(0);
-var beginbtn = document.querySelector("#Begin-btn");
-beginbtn.addEventListener("click", defaultshow);
-
-function defaultshow(e) {
-  console.log(e.target);
-  show();
-}
+];
